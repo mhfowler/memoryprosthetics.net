e800bac52433a7c963e987c1f50d75c9e83748b2
Max Fowler <maxhfowler@gmail.com>
2017-01-05 20:00:11 -0500

    desktop only

--- js/google-docs-site.1.0.js
+++ js/google-docs-site.1.0.js
@@ -68,7 +68,7 @@ $(function() {
 
 	var window_width = $(window).width();
 	if (window.mobilecheck() === true) {
-			window.location = "https://gist.githubusercontent.com/mhfowler/d611991cbe02b2a118fb2d17bda4f481/raw/f106568390238f41cf231ff9adeacb8737b911ef/MEMORY%2520PROSTHETICS"
+			// window.location = "https://gist.githubusercontent.com/mhfowler/d611991cbe02b2a118fb2d17bda4f481/raw/f106568390238f41cf231ff9adeacb8737b911ef/MEMORY%2520PROSTHETICS"
 	}
 
 	//little fix for the iframe size on mobile
